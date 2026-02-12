fe22195e10d873c56d61f772159a841310e93810
Aaron1803 <aarononorez2006@hotmail.com>
2026-02-12 13:33:00 -0500

    Finalizacion de ejercicio 3

---
+++ Ejercicio_3.py
@@ -0,0 +1,8 @@
+numero = int(input("Ingrese un número entero: "))
+
+cuadrado = numero ** 2
+cubo = numero ** 3
+
+print(f"\nNúmero: {numero}")
+print(f"Cuadrado: {cuadrado}")
+print(f"Cubo: {cubo}")
